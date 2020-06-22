d70da25f7588ad3d53f41468c5e8855d1e2aa2b9
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-22 19:01:45 +0000

    Done.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,23 @@
+class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def home
+  end
+
+  def create
+    if params[:name].nil? || params[:name].empty?
+      redirect_to '/new'
+    else
+      session[:name] = params[:name]
+      redirect_to '/home'
+    end
+  end
+
+  def destroy
+    session[:name] = nil
+    redirect_to '/new'
+  end
+
+end
